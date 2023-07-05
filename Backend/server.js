57a24b139de7d4d324964d91788fb5489e4941ae
DhruvKumar Nagar <iamdhruvnagar@gmail.com>
2023-07-05 21:37:15 +0530

    Basic setup done for frontend and backend

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const notes = require("../Backend/data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server is started on nodejs project");
+});
